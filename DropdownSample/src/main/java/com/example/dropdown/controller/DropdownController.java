b3bcef6166d6dd56cbdc2830317d1469346cb3c9
aojirutaishi <97506927+aojirutaishi@users.noreply.github.com>
2022-01-11 16:51:16 +0900

    Add files via upload

---
+++ DropdownSample/src/main/java/com/example/dropdown/controller/DropdownController.java
@@ -0,0 +1,21 @@
+package com.example.dropdown.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class DropdownController {
+	
+	public DropdownController() {
+		
+	}
+	
+	@GetMapping("/")
+	public String initialize() {
+		
+		return "calendar.scala";
+	}
+	
+}
