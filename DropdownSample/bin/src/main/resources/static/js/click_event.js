fefd383bd33fe498fd9c05d06a785f9e7df0e5b3
dan-tbrave <dan-t@tbrave.com>
2022-01-13 14:40:51 +0900

    とりあえず全部追加
    Signed-off-by: dan-tbrave <dan-t@tbrave.com>

---
+++ DropdownSample/bin/src/main/resources/static/js/click_event.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+ $('.dm_y').on( 'click',  function() {
+	alert('クリックされました');
+/*    var text = $(this).html();
+    var htmlText = text + ' <span class="caret y"></span>';
+    $(this).closest('.dm_y').find('.dropdown-toggle').html(htmlText);
+    document.getElementById("id_year").value = text;*/
+});
